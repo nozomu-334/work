9ba407a3c38e63c3884c854c65116f56d0d2a0b4
nozomushimizu <nozomushimizu@localhost>
2025-05-12 16:03:50 +0900

    最初のコミット

---
+++ src/sample/sample.java
@@ -0,0 +1,36 @@
+package sample;
+
+public class sample {
+
+	public static void main(String[] args) {
+		System.out.println("Java 学習中");
+	
+		int age = 20;
+		String name = "たろう";
+		int x = 4;
+		int y = 6;
+		
+		System.out.println("縦幅"+x+"横幅"+y+"の長方形の面積は"+x*y);
+		
+		boolean ww = false;
+		char chch = 'A';
+		float shosu = 6.28f;
+		long numb = 123456789012345L;
+		String word = "プログラミングは楽しい！";
+		
+		int a = 5;
+		int b = 2;
+		int result = a / b;
+		
+		System.out.println(age);
+		System.out.println(name);
+		System.out.println(ww);
+		System.out.println(chch);
+		System.out.println(shosu);
+		System.out.println(numb);
+		System.out.println(word);
+		System.out.println(result);
+
+	}
+
+}
