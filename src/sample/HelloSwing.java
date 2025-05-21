b73153c02a6b4866690f4fef95b1686fdda38127
nozomushimizu <nozomushimizu@ip-10-95-1-206.ap-northeast-1.compute.internal>
2025-05-21 12:55:21 +0900

    5/21 再

--- src/sample/HelloSwing.java
+++ src/sample/HelloSwing.java
@@ -1,10 +1,14 @@
 package sample;
 
+import java.awt.BorderLayout;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
 public class HelloSwing {
@@ -17,12 +21,26 @@ public class HelloSwing {
             // ラベルに文字列を設定
             JLabel label = new JLabel("ようこそJavaの世界へ", JLabel.CENTER);
             
-            JButton button = new JButton("OK");
+            JButton button = new JButton("OK?");
+            
+            button.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    label.setText("OK!");
+                }
+            });
             
             label.setFont(new Font("Arial", Font.PLAIN, 20));
             
+            JPanel panel = new JPanel();
+            panel.setLayout(new BorderLayout());
+            panel.add(label, BorderLayout.CENTER);
+            panel.add(button, BorderLayout.SOUTH);
+            
             // フレームにラベルを追加
             frame.add(label);
+            frame.setLocationRelativeTo(null);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setVisible(true);
 
             // フレームのサイズを設定
             frame.setSize(400, 200);
